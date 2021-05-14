1522c285028f8dce986186e8159f321a037d3985
dhivin456 <74041229+dhivin456@users.noreply.github.com>
2021-05-14 13:39:44 +0530

    Add files via upload

---
+++ HumanPlayer.java
@@ -0,0 +1,10 @@
+package Chess;
+
+public class HumanPlayer extends Player {
+
+	public HumanPlayer(boolean whiteSide)
+	{
+		this.whiteSide = whiteSide;
+		this.humanPlayer = true;
+	}
+}
---
+++ Player.java
@@ -0,0 +1,20 @@
+package Chess;
+
+public abstract class Player {
+	public boolean whiteSide;
+	public boolean humanPlayer;
+
+	public boolean isWhiteSide()
+	{
+		return this.whiteSide;
+	}
+	public boolean isHumanPlayer()
+	{
+		return this.humanPlayer;
+	}
+}
+
+
+
+
+
